430a0d1a6d6f655daa86b45783a4dd881c1e4972
confett0 <118047961+confett0@users.noreply.github.com>
2024-08-29 18:10:09 +0200

    Display products in shop page

---
+++ components/ProductCard.jsx
@@ -0,0 +1,9 @@
+export default function ProductCard({image, name, price }) {
+    return (
+        <div className="product-card">
+            <img src={image} />
+            <h3>{name}</h3>
+            <p>{price}</p>
+        </div>
+    )
+}
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,5 +1,23 @@
+import { useState, useEffect } from "react";
+import ProductCard from "../../components/ProductCard" 
+
 export default function Shop() {
+
+    const [data, setData] = useState([]);
+    console.log(data)
+
+    useEffect(() => {
+        fetch("src/products.json")
+        .then(res => res.json())
+        .then(data => setData(data.products))
+    }, [])
+
+    const productElements = data.map(item => <ProductCard key={item.id} image={item.image} name={item.name} price={item.price} />)
+
     return (
-        <h1>Shop goes here</h1>
+        <>
+        <h1>Shop our products</h1>
+        {productElements}
+        </>
     )
 }
