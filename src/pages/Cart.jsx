4c99140bf0b0a22172ee8731851cedce588b16d0
confett0 <118047961+confett0@users.noreply.github.com>
2024-09-04 20:37:37 +0200

    Add order total to cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,16 +1,20 @@
 export default function Cart({ cart }) {
+  const orderTotal = cart.reduce((a, b) => a + b.price, 0);
+
   const itemElements = cart.map((item) => (
     <div key={item.id} className="cart-item">
-        <img src={item.image} />
+      <img src={item.image} />
       <h4>{item.name}</h4>
       <p>{item.quantity}</p>
       <p>{item.price}</p>
     </div>
   ));
+
   return (
     <>
       <h2>Your cart</h2>
       {itemElements}
+      <p>Total: {orderTotal}</p>
     </>
   );
 }
