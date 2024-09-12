02cf449f1d90c4fa2192f5d4f895cfebe3dce5dd
confett0 <118047961+confett0@users.noreply.github.com>
2024-09-12 18:25:31 +0200

    Style and add contente to footer

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,19 +8,42 @@ export default function Layout({ totalItems }) {
           <img src="src/assets/shared/desktop/logo.svg" />
         </Link>
         <nav>
-        <NavLink to="/">Home</NavLink>
+          <NavLink to="/">Home</NavLink>
           <NavLink to="shop">Shop</NavLink>
+          <NavLink to="">Headphones</NavLink>
+          <NavLink to="">Speakers</NavLink>
+          <NavLink to="">Earphones</NavLink>
         </nav>
         <Link to="cart">
-            <img src="src/assets/shared/desktop/icon-cart.svg" />{" "}
-            <span className="total-items">{totalItems ? totalItems : ""}</span>
-          </Link>
+          <img src="src/assets/shared/desktop/icon-cart.svg" />{" "}
+          <span className="total-items">{totalItems ? totalItems : ""}</span>
+        </Link>
       </header>
       <div className="outlet">
         <Outlet />
       </div>
       <footer>
-        <p>footer content</p>
+        <div className="footer-wrap">
+          <img src="src/assets/shared/desktop/logo.svg" />
+          <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="">Headphones</NavLink>
+            <NavLink to="">Speakers</NavLink>
+            <NavLink to="">Earphones</NavLink>
+          </nav>
+          <p>
+            Audiophile is an all in one stop to fulfill your audio needs. We're a
+            small team of music lovers and sound specialists who are devoted to
+            helping you get the most out of personal audio. Come and visit our
+            demo facility - we’re open 7 days a week.
+          </p>
+          <div className="social-icons">
+            <img src="src/assets/shared/desktop/icon-facebook.svg" />
+            <img src="src/assets/shared/desktop/icon-twitter.svg" />
+            <img src="src/assets/shared/desktop/icon-instagram.svg" />
+          </div>
+          <p>Copyright 2021. All Rights Reserved</p>
+        </div>
       </footer>
     </>
   );
