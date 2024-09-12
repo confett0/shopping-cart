0a19aa05ab8f8f29fd4ac525414e35cfa3459a3c
confett0 <118047961+confett0@users.noreply.github.com>
2024-09-12 20:55:57 +0200

    Style button

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -26,16 +26,16 @@ export default function Layout({ totalItems }) {
         <div className="footer-wrap">
           <img src="../src/assets/shared/desktop/logo.svg" />
           <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="">Headphones</NavLink>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="">Headphones</NavLink>
             <NavLink to="">Speakers</NavLink>
             <NavLink to="">Earphones</NavLink>
           </nav>
           <p>
-            Audiophile is an all in one stop to fulfill your audio needs. We're a
-            small team of music lovers and sound specialists who are devoted to
-            helping you get the most out of personal audio. Come and visit our
-            demo facility - we’re open 7 days a week.
+            Audiophile is an all in one stop to fulfill your audio needs. We're
+            a small team of music lovers and sound specialists who are devoted
+            to helping you get the most out of personal audio. Come and visit
+            our demo facility - we’re open 7 days a week.
           </p>
           <div className="social-icons">
             <img src="../src/assets/shared/desktop/icon-facebook.svg" />
