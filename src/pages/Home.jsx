cbbe2ac304ea02e932f9d90b7a2aebf3c7313a65
confett0 <118047961+confett0@users.noreply.github.com>
2024-09-13 18:44:40 +0200

    Style hero area background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ export default function Home() {
   return (
     <>
       <section className="hero-area">
-          <h5>New Product</h5>
-          <h2>XX99 Mark II Headphones</h2>
-          <p>
-            Experience natural, lifelike audio and exceptional build quality made
-            for the passionate music enthusiast.
-          </p>
-          <button>See product</button>
+          <div className="home-product-card">
+            <h5>New Product</h5>
+            <h1>XX99 Mark II Headphones</h1>
+            <p>
+              Experience natural, lifelike audio and exceptional build quality made
+              for the passionate music enthusiast.
+            </p>
+            <button>See product</button>
+        </div>
       </section>
     </>
   );
