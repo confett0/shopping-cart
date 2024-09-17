872a743baf173e6f3403b9fd1d07e8db704b7d83
confett0 <118047961+confett0@users.noreply.github.com>
2024-09-17 20:34:22 +0200

    Style product page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -27,45 +27,48 @@ export default function ProductPage({
   }
 
   const includedItemElements = productData.includedItems.map(item => <ul key={item.item}>
-    <li>{item.item} x {item.quantity}</li>
+    <li><span className="orange-text">{item.quantity}x</span> {item.item}</li>
   </ul>);
 
   return (
     <div className="content-wrap product-page">
       <img src={productData.image} />
       <div className="product-info">
+      {productData.new && <p className="overline orange-text">New product</p>}
         <h2>{productData.name}</h2>
         <p>{productData.description}</p>
-        <p>{productData.price}</p>
-        <div className="quantity-wrap">
-          <button className="quantity-button" onClick={decrementQuantity}>
-            -
-          </button>
-          <input
-            type="number"
-            className="item-quantity"
-            name="item-quantity"
-            value={itemQuantity}
-            onChange={(e) => {
-              if (e.target.value === "") {
-                // avoid displaying 0 when erasing value inside the input
-                setItemQuantity("");
-              } else {
-                setItemQuantity(+e.target.value);
-              }
-            }}
-          />
-          <button className="quantity-button" onClick={incrementQuantity}>
-            +
-          </button>
+        <h6>$ {productData.price}</h6>
+        <div className="button-wrap">
+          <div className="quantity-wrap">
+            <button className="quantity-button" onClick={decrementQuantity}>
+              -
+            </button>
+            <input
+              type="number"
+              className="item-quantity"
+              name="item-quantity"
+              value={itemQuantity}
+              onChange={(e) => {
+                if (e.target.value === "") {
+                  // avoid displaying 0 when erasing value inside the input
+                  setItemQuantity("");
+                } else {
+                  setItemQuantity(+e.target.value);
+                }
+              }}
+            />
+            <button className="quantity-button" onClick={incrementQuantity}>
+              +
+            </button>
+          </div>
+          <button className="orange" onClick={() => addToCart(productData)}>Add to cart</button>
+                </div>
         </div>
-        <button onClick={() => addToCart(productData)}>Add to cart</button>
-      </div>
-      <div>
+      <div className="product-features">
         <h3>Features</h3>
         <p>{productData.features}</p>
       </div>
-      <div>
+      <div className="product-included">
         <h3>Included items</h3>
         {includedItemElements}
       </div>
