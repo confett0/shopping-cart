f34f3052bf50e852020bb6c8ff4abdc9824503c6
confett0 <118047961+confett0@users.noreply.github.com>
2024-09-30 20:26:04 +0200

    Add product links to homepage

--- src/components/ItemQuantitySelector.jsx
+++ src/components/ItemQuantitySelector.jsx
@@ -1,26 +1,31 @@
-export default function ItemQuantitySelector({itemQuantity, incrementQuantity, decrementQuantity, setItemQuantity}) {
-    return (
-        <div className="quantity-wrap">
-          <button className="quantity-button" onClick={decrementQuantity}>
-            -
-          </button>
-          <input
-            type="number"
-            className="item-quantity"
-            name="item-quantity"
-            value={itemQuantity}
-            onChange={(e) => {
-              if (e.target.value === "") {
-                // avoid displaying 0 when erasing value inside the input
-                setItemQuantity("");
-              } else {
-                setItemQuantity(+e.target.value);
-              }
-            }}
-          />
-          <button className="quantity-button" onClick={incrementQuantity}>
-            +
-          </button>
-          </div>
-    )
+export default function ItemQuantitySelector({
+  itemQuantity,
+  incrementQuantity,
+  decrementQuantity,
+  setItemQuantity,
+}) {
+  return (
+    <div className="quantity-wrap">
+      <button className="quantity-button" onClick={decrementQuantity}>
+        -
+      </button>
+      <input
+        type="number"
+        className="item-quantity"
+        name="item-quantity"
+        value={itemQuantity}
+        onChange={(e) => {
+          if (e.target.value === "") {
+            // avoid displaying 0 when erasing value inside the input
+            setItemQuantity("");
+          } else {
+            setItemQuantity(+e.target.value);
+          }
+        }}
+      />
+      <button className="quantity-button" onClick={incrementQuantity}>
+        +
+      </button>
+    </div>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import CategoryLinks from "../components/CategoryLinks";
 import BeforeFooterArea from "../components/BeforeFooterArea";
 
@@ -12,39 +13,47 @@ export default function Home() {
             Experience natural, lifelike audio and exceptional build quality
             made for the passionate music enthusiast.
           </p>
-          <button className="orange">See product</button>
+          <Link to="/shop/xx99-mark-two-headphones">
+            <button className="orange">See product</button>
+          </Link>
         </div>
       </section>
-        <div className="content-wrap">
-          <CategoryLinks />
-                <section className="orange-area area">
-                <img src="./src/assets/home/desktop/image-speaker-zx9.png" />
+      <div className="content-wrap">
+        <CategoryLinks />
+        <section className="orange-area area">
+          <img src="./src/assets/home/desktop/image-speaker-zx9.png" />
           <div className="home-product-card">
             <h1>ZX9 SPEAKER</h1>
             <p>
               Upgrade to premium speakers that are phenomenally built to deliver
               truly remarkable sound.
             </p>
-            <button className="black">See product</button>
+            <Link to="/shop/zx9-speaker">
+              <button className="black">See product</button>
+            </Link>
           </div>
-                </section>
-                <section className="grey-area area">
+        </section>
+        <section className="grey-area area">
           <div className="home-product-card">
             <h4>ZX7 SPEAKER</h4>
-            <button className="clear">See product</button>
+            <Link to="/shop/zx7-speaker">
+              <button className="clear">See product</button>
+            </Link>
           </div>
-                </section>
-                <section className="black-area area">
+        </section>
+        <section className="black-area area">
           <img src="./src/assets/home/desktop/image-earphones-yx1.jpg" />
           <div className="card-wrap">
             <div className="home-product-card">
               <h4>YX1 EARPHONES</h4>
-              <button className="clear">See product</button>
+              <Link to="/shop/yx1-earphones">
+                <button className="clear">See product</button>
+              </Link>
             </div>
           </div>
-                </section>
-                <BeforeFooterArea />
-        </div>
+        </section>
+        <BeforeFooterArea />
+      </div>
     </>
   );
 }
