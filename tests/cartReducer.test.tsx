a6c04067458f7ba6775942ef386b400c3017dd0e
confett0 <118047961+confett0@users.noreply.github.com>
2026-05-15 18:24:05 +0200

    Add tests for cartReducer

--- tests/cartReducer.test.tsx
+++ tests/cartReducer.test.tsx
@@ -10,13 +10,14 @@ import type { CartItem } from "../src/types/cart";
  * non muta lo state originale
  */
 
+const mockProduct = {
+  id: 1,
+  name: "Test Product",
+  price: 100,
+};
+
 it("adds a new item to cart", () => {
   const state: CartItem[] = [];
-  const mockProduct = {
-    id: "1",
-    name: "Test Product",
-    price: 100,
-  };
 
   const result = cartReducer(state, {
     type: "ADDED_ITEM",
@@ -26,4 +27,25 @@ it("adds a new item to cart", () => {
 
   expect(result).toHaveLength(1);
   expect(result[0]?.quantity).toBe(1);
+});
+
+it("increments quantity if product is already in cart", () => {
+  const state: CartItem[] = [{ ...mockProduct, quantity: 1 }];
+  const result = cartReducer(state, {
+    type: "ADDED_ITEM",
+    payload: mockProduct,
+    quantity: 1,
+  });
+  expect(result[0]?.quantity).toBe(2);
+  expect(result).toHaveLength(1);
+});
+
+it("resets cart", () => {
+  const state: CartItem[] = [
+    { id: 1, name: "Test Product", price: 100, quantity: 1 },
+  ];
+  const result = cartReducer(state, {
+    type: "EMPTIED_CART",
+  });
+  expect(result).toHaveLength(0);
 });
